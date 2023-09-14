8b93e55f218a6dd86caada5d9dc843303a526c08
cy <limcaiyin99@gmail.com>
2023-09-14 15:42:23 +0800

    refactored sidepanel to make it reusable

--- frontend/src/Pages/Orders.jsx
+++ frontend/src/Pages/Orders.jsx
@@ -3,74 +3,15 @@ import Stack from '@mui/system/Stack';
 import RouteHistory from "../Components/RouteHistory";
 import Button from '@mui/material/Button';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-
+import SidePanel from "../Components/SidePanel";
   
 const Orders = () => {
   return (
     <>
         <Grid container spacing={0} style={{height: '80vh' }}>
-            <Grid item xs={3.5} sx={{ display: { xs: 'none', md: 'inline' }}} style={{ backgroundColor: '#e4dccd', height: '80vh' }}>
-                <RouteHistory page="orders" routeName="orders"/>
-                <Stack 
-                    direction={{ xs: 'column'}}
-                    spacing={{ xs: 2}}
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                >
-                    <h3>Hi, Mary</h3>
-                    <Button 
-                        variant="contained"
-                        endIcon={<ArrowForwardIosIcon />}
-                        href="/orders"
-                        sx ={{
-                            backgroundColor:"#75695a",
-                            boxShadow: "none",
-                            minWidth: "200px",
-                            minHeight: "50px",
-                            '&:hover': {
-                                backgroundColor: '#e4dccd',
-                                color:"#75695a",
-                                opacity: [0.9, 0.8, 0.7],
-                            }
-                        }}>
-                    Orders
-                    </Button>
-                    <Button 
-                        variant="contained"
-                        endIcon={<ArrowForwardIosIcon />}
-                        href="/orders"
-                        sx ={{
-                            backgroundColor:"#75695a",
-                            boxShadow: "none",
-                            minWidth: "200px",
-                            minHeight: "50px",
-                            '&:hover': {
-                                backgroundColor: '#e4dccd',
-                                color:"#75695a",
-                                opacity: [0.9, 0.8, 0.7],
-                            }
-                        }}>
-                    Personal Data
-                    </Button>
-                    <Button 
-                        variant="contained"
-                        endIcon={<ArrowForwardIosIcon />}
-                        href="/orders"
-                        sx ={{
-                            backgroundColor:"#75695a",
-                            boxShadow: "none",
-                            minWidth: "200px",
-                            minHeight: "50px",
-                            '&:hover': {
-                                backgroundColor: '#e4dccd',
-                                color:"#75695a",
-                                opacity: [0.9, 0.8, 0.7],
-                            }
-                        }}>
-                    Sign Out
-                    </Button>
-                </Stack>
-            </Grid>
+            {/* refactored */}
+            <SidePanel page={"orders"} route={"orders"}/>
+            {/* refactored */}
             <Grid item xs={8.5} style={{ backgroundColor: '#f3efe7', height: '80vh' }}>
                 <Stack 
                     direction={{ xs: 'column'}}
