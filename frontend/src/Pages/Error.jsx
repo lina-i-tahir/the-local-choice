6fbfc8fd0768310f68b6b16a01f9779933697103
lina-i-tahir <lina.i.tahir@gmail.com>
2023-09-14 13:38:06 +0800

    updated "page not found"

--- frontend/src/Pages/Error.jsx
+++ frontend/src/Pages/Error.jsx
@@ -1,5 +1,24 @@
+import { Typography } from "@mui/material";
 const Error = () => {
-  return <h1>Page not found</h1>;
+  return (
+    <Typography
+      variant="h6"
+      noWrap
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        fontFamily: "Poppins",
+        fontWeight: 500,
+        color: "#414B3B",
+        textDecoration: "none",
+        margin: "300px 0px",
+        fontSize: "26px",
+        textAlign: "center",
+      }}
+    >
+      Page not found
+    </Typography>
+  );
 };
 
 export default Error;
