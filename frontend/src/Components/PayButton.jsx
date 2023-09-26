ae6d0751fa4bc9a40c9f24629aadf672f68d2089
lina-i-tahir <lina.i.tahir@gmail.com>
2023-09-27 00:33:41 +0800

    + items showing on checkout page

--- frontend/src/Components/PayButton.jsx
+++ frontend/src/Components/PayButton.jsx
@@ -1,17 +1,30 @@
 import { Button } from "@mui/material";
 import axios from "axios";
 import { BASE_URL } from "../constants";
+import { useSelector } from "react-redux";
 
 // const token = localStorage.getItem("token");
 
-const PayButton = ({ cartItems }) => {
+const PayButton = () => {
   const url = BASE_URL;
 
   const userData = localStorage.getItem("user");
   const user = JSON.parse(userData);
   console.log(user._id);
 
+  const { cartItems } = useSelector((state) => state.cart);
+  // const { totalPrice } = useSelector((state) => state.cart); // total price of all items in cart
+  // const totalQty = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+
   const handleCheckout = () => {
+    console.log("cart item: " + cartItems, "user id: " + user._id);
+    if (!cartItems || !Array.isArray(cartItems) || cartItems.length === 0) {
+      // Handle the case when cartItems is empty or not in the expected format
+      console.error("Invalid cartItems data.");
+      return;
+    }
+
+    // Proceed with making the POST request
     axios
       .post(`${url}/stripe/create-checkout-session`, {
         cartItems,
@@ -23,8 +36,8 @@ const PayButton = ({ cartItems }) => {
         }
       })
       .catch((err) => console.log(err.message));
-    console.log(cartItems, user._id);
   };
+
   return (
     <>
       <Button
@@ -50,3 +63,57 @@ const PayButton = ({ cartItems }) => {
 };
 
 export default PayButton;
+
+// original
+// import { Button } from "@mui/material";
+// import axios from "axios";
+// import { BASE_URL } from "../constants";
+
+// // const token = localStorage.getItem("token");
+
+// const PayButton = ({ cartItems }) => {
+//   const url = BASE_URL;
+
+//   const userData = localStorage.getItem("user");
+//   const user = JSON.parse(userData);
+//   console.log(user._id);
+
+//   const handleCheckout = () => {
+//     axios
+//       .post(`${url}/stripe/create-checkout-session`, {
+//         cartItems,
+//         userId: user._id,
+//       })
+//       .then((res) => {
+//         if (res.data.url) {
+//           window.location.href = res.data.url;
+//         }
+//       })
+//       .catch((err) => console.log(err.message));
+//     console.log("cart item: " + cartItems, +"user " + user._id);
+//   };
+//   return (
+//     <>
+//       <Button
+//         variant="contained"
+//         color="primary"
+//         onClick={() => handleCheckout()}
+//         sx={{
+//           backgroundColor: "#99958C",
+//           color: "#E4DCCD",
+//           width: "20ch",
+//           height: "70px",
+//           textAlign: "center",
+//           padding: "18px",
+//           "&:hover": {
+//             backgroundColor: "#737373",
+//           },
+//         }}
+//       >
+//         proceed to payment
+//       </Button>
+//     </>
+//   );
+// };
+
+// export default PayButton;
