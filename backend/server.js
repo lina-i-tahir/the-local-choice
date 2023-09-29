b72589ff34210a7b96892697f165bf3816d556d2
cy <limcaiyin99@gmail.com>
2023-09-29 23:29:46 +0800

    cors

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json({ limit: "50mb" }));
 // Set up CORS to allow React app to make requests to this API
+app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -48,13 +49,12 @@ app.use("/profile", profileRouter);
 
 app.use("/", stripeRouter);
 
-const corsOptions = {
-  origin: 'https://localhost:3000', // or you can use a function to dynamically allow origins
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // allow session cookie from browser to pass through
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
