237c1724ffae6df9582a3ce38fdb29ef3c5648b2
cy <limcaiyin99@gmail.com>
2023-09-29 23:34:52 +0800

    cors

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
   })
 );
 // view engine setup
