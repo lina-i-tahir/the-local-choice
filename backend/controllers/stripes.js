d578ea19c28899eec94b03866c976cd717f67d27
lina-i-tahir <lina.i.tahir@gmail.com>
2023-10-07 16:11:25 +0800

    update inventory

--- backend/controllers/stripes.js
+++ backend/controllers/stripes.js
@@ -1,37 +1,37 @@
-const orderModel  = require("../models/orderModel");
+const orderModel = require("../models/orderModel");
 const User = require("../models/user");
-const Store = require('../models/store');
+const Store = require("../models/store");
 
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = {
-    createCheckoutSession,
-    createOrder,
+  createCheckoutSession,
+  createOrder,
 };
 
-async function createCheckoutSession(req,res) {
-  const { cartItems, userId } = req.body; 
+async function createCheckoutSession(req, res) {
+  const { cartItems, userId } = req.body;
 
   if (!Array.isArray(cartItems) || cartItems.length === 0) {
     return res.status(400).json({ error: "Cart is empty." });
   }
-  
+
   // customise data, keep only essential information for metadata
-  const metadataCartItems = cartItems.map(item => {
+  const metadataCartItems = cartItems.map((item) => {
     return {
-        name: item.name,
-        id: item._id, 
-        quantity: item.quantity,
-        price: item.price,
-        storeId: item.storeId,
+      name: item.name,
+      id: item._id,
+      quantity: item.quantity,
+      price: item.price,
+      storeId: item.storeId,
     };
   });
 
   // check if metadata is too large
   const metadataString = JSON.stringify({ userId, cart: metadataCartItems });
   if (metadataString.length > 500) {
-      return res.status(400).json({ error: "Metadata is too large." });
+    return res.status(400).json({ error: "Metadata is too large." });
   }
 
   // create customer
@@ -55,15 +55,15 @@ async function createCheckoutSession(req,res) {
       unit_amount: Math.round(item.price * 100), // Convert the unit price to cents and round up because of the decimal
     },
 
-    quantity: item.quantity, 
+    quantity: item.quantity,
   }));
-  
+
   // create stripe checkout session
   const session = await stripe.checkout.sessions.create({
     shipping_address_collection: {
       allowed_countries: ["SG"],
     },
-    line_items: lineItems, 
+    line_items: lineItems,
     customer: customer.id,
     mode: "payment",
     success_url: `${process.env.CLIENT_URL}/scsuccess/session_id={CHECKOUT_SESSION_ID}`,
@@ -77,8 +77,8 @@ async function createOrder(req, res) {
   try {
     // get session id to retrieve session details
     let { session_id } = req.params;
-    if (session_id.startsWith('session_id=')) {
-      session_id = session_id.replace('session_id=', ''); // remove the prefix
+    if (session_id.startsWith("session_id=")) {
+      session_id = session_id.replace("session_id=", ""); // remove the prefix
     }
 
     console.log("Corrected Session ID:", session_id);
@@ -87,23 +87,25 @@ async function createOrder(req, res) {
     const customer = await stripe.customers.retrieve(session.customer);
 
     if (!customer.metadata || !customer.metadata.cart) {
-      return res.status(400).json({ errorMsg: "Cart data is missing in customer metadata" });
+      return res
+        .status(400)
+        .json({ errorMsg: "Cart data is missing in customer metadata" });
     }
 
     const cartData = JSON.parse(customer.metadata.cart);
     const cartItems = cartData.cart;
 
     // pass data into order model in correct format
-    const formattedOrderItems = cartItems.map(item => ({
+    const formattedOrderItems = cartItems.map((item) => ({
       name: item.name,
       id: item.id,
       qty: item.quantity,
-      price: item.price
+      price: item.price,
     }));
 
-    console.log("customer", customer)
-    console.log("session", session)
-    console.log("cartItems", cartItems)
+    console.log("customer", customer);
+    console.log("session", session);
+    console.log("cartItems", cartItems);
 
     const order = new orderModel({
       user: customer.metadata.userId,
@@ -111,7 +113,10 @@ async function createOrder(req, res) {
       orderId: customer.invoice_prefix,
       orderItems: formattedOrderItems,
       shippingAddress: {
-        address: session.shipping_details.address.line1 + " " + session.shipping_details.address.line2,
+        address:
+          session.shipping_details.address.line1 +
+          " " +
+          session.shipping_details.address.line2,
         postalCode: session.shipping_details.address.postal_code,
         country: session.shipping_details.address.country,
       },
@@ -125,21 +130,18 @@ async function createOrder(req, res) {
     });
 
     // update the store inventory
-    cartItems.forEach(async (item) => {
-      const store = await Store.findById(item.storeId);
-      const product = store.products.id(item.id); 
-      product.quantity -= item.quantity; // update the quantity
 
-      await store.save(); // save the store
-    });
-    
+    for (const item of cartItems) {
+      const store = await Store.findById(item.storeId);
+      const product = store.products.id(item.id);
+      product.quantity -= item.quantity;
+      await store.save();
+    }
+
     await order.save();
     res.status(201).json({ title: "Update and create order", order });
-
   } catch (err) {
     console.log(err);
     res.status(500).json({ errorMsg: err.message });
   }
 }
-
-
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,10 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from 'react-redux';
-import reduxStore from './reduxStore';
+import { Provider } from "react-redux";
+import reduxStore from "./reduxStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
